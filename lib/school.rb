fe041a9a5ec42bd56f23c66e29a2664644cfe486
github username <github email address>
2016-09-29 13:27:07 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+require 'Pry'
+class School
+attr_accessor 
+attr_reader :schoo, :roster
+
+def initialize(schoo, roster={})
+  @schoo = schoo
+  @roster = roster 
+end 
+
+def add_student(student, grade)
+  @student = student
+  @grade = grade
+
+ if @roster.keys.include?(@grade)
+  @roster[@grade] << @student
+else   
+  @roster[@grade] = []
+  @roster[@grade] << @student
+end 
+end
+
+def grade(grade)
+  @grade = grade
+  @roster[@grade]
+
+end 
+
+def sort
+  @roster.each_with_object({}) do |(grade, students), new_hash|
+    new_hash[grade] = students.sort 
+  end
+end
+
+  # binding.pry
+
+# puts "yo"
+end 
